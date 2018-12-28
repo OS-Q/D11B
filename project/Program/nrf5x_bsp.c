596eb605c7fbbd8f3a295a2fc804a42063e82c48
qitas <qitas@qitas.cn>
2018-12-28 17:51:58 +0800

    add project

---
+++ project/Program/nrf5x_bsp.c
@@ -0,0 +1,44 @@
+/* Includes --------------------------------------------------------------------------------*/
+#include "drivers\nrf5x_system.h"
+#include "drivers\nrf5x_clock.h"
+#include "modules\serial.h"
+#include "nrf5x_bsp.h"
+
+/** @addtogroup NRF5x_Program
+ *  @{
+ */
+
+/* Private typedef -------------------------------------------------------------------------*/
+/* Private define --------------------------------------------------------------------------*/
+/* Private macro ---------------------------------------------------------------------------*/
+/* Private variables -----------------------------------------------------------------------*/
+/* Private function prototypes -------------------------------------------------------------*/
+/* Private functions -----------------------------------------------------------------------*/
+
+void BSP_CLOCK_Config( void )
+{
+  CLOCK_Config();
+}
+
+void BSP_GPIO_Config( void )
+{
+  nrf_gpio_cfg_output(LED_R_PIN);
+  nrf_gpio_cfg_output(LED_G_PIN);
+  nrf_gpio_cfg_output(LED_B_PIN);
+  nrf_gpio_cfg_input(KEY_PIN, NRF_GPIO_PIN_NOPULL);
+
+  LED_R_Set();
+  LED_G_Set();
+  LED_B_Set();
+}
+
+void BSP_UART_SERIAL_Config( pFunc rxEvent )
+{
+  hSerial.RxEventCallback = rxEvent;
+
+  Serial_Config();
+//  kSerial_Config(SERIAL_UARTx);
+  printf("\r\nHello World!\r\n\r\n");
+}
+
+/*************************************** END OF FILE ****************************************/
